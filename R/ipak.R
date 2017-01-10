63549f075617017d850cba29324ad08bb31ff21b
callinSwitzer <callin.switzer@gmail.com>
2017-01-10 16:19:56 -0500

    attributed function to author

--- R/ipak.R
+++ R/ipak.R
@@ -1,5 +1,7 @@
 #install packages
 ipak <- function(pkg){
+     # function by stevenworthington
+     # https://gist.github.com/stevenworthington/3178163
      new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
      if(length(new.pkg)) install.packages(new.pkg, dependencies = TRUE)
      sapply(pkg, require, character.only = TRUE)
